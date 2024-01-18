883dd28287d595275df1db4132e798290fb0b847
iLuzzio <78630605+iLuzzio@users.noreply.github.com>
2024-01-18 15:19:43 +0000

    change code

--- Hello.py
+++ Hello.py
@@ -1,51 +1,89 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import streamlit
+from newspaper import Article
+import requests
 
-import streamlit as st
-from streamlit.logger import get_logger
+streamlit.set_page_config(page_title='KASIH NAMA WEB')
 
-LOGGER = get_logger(__name__)
+apiKey = 'd9addde433a74562aa80be87342bb984'
 
+#Berita teratas indonesia
+def berita_teratas():
+    url = 'https://newsapi.org/v2/top-headlines'
+    params = {
+        'country': 'id',
+        'apiKey': apiKey,
+        'pageSize': 25,
+        'sortBy' : 'relevancy'
+    }
+    response = requests.get(url, params=params)
+    news_list = response.json().get('articles', [])
+    return news_list
+#Mencari berita
+def cari_berita(topic):
+    url = 'https://newsapi.org/v2/everything'
+    params = {
+        'q': topic,
+        'language': 'id',
+        'apiKey': apiKey,
+        'pageSize': 25
+    }
+    response = requests.get(url, params=params)
+    news_list = response.json().get('articles', [])
+    return news_list
+
+def display_news(news_list, articles_per_page):
+    c = 0
+    for news in news_list:
+        c += 1
+        streamlit.write('**({}) {}**'.format(c, news['title']))
+        news_data = Article(news['url'])
+        try:
+            news_data.download()
+            news_data.parse()
+            news_data.nlp()
+        except Exception as e:
+            print(e)
+        with streamlit.expander(news['title']):
+            if news['author']:
+                streamlit.write(news['author'])
+            streamlit.markdown(
+                '''<h6 style="text-align: justify;">{}</h6>'''.format(news_data.summary),
+                unsafe_allow_html=True
+            )
+            if news['urlToImage']:
+                streamlit.image(news['urlToImage'])
+            streamlit.markdown("[Baca Selengkapnya {}...]({})".format(news['source']['name'], news['url']))
+        streamlit.success("Tanggal Terbit: " + news['publishedAt'])
+        if c >= articles_per_page:
+            break
 
 def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+    streamlit.title("KASIH NAMA WEB")
+    category = ['Pilihan Kategori', 'Artikel Teratas', 'Cari Artikel']
+    category_options = streamlit.selectbox('Pilih Kategori', category)
 
-    st.write("# Welcome to Streamlit! 👋")
+    if category_options == category[0]:
+        streamlit.warning('Untuk mendapatkan Artikel pilih salah satu kategori')
 
-    st.sidebar.success("Select a demo above.")
+    elif category_options == category[1]:
+        streamlit.subheader(" Berikut merupakan beberapa artikel teratas ")
+        articles_per_page = streamlit.slider("Jumlah artikel yang ditampilkan", min_value=5, max_value=25, step=5)
+        news_list = berita_teratas()
+        display_news(news_list, articles_per_page)
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
+    elif category_options == category[2]:
+        user_topic = streamlit.text_input("Cari Artikel")
+        articles_per_page = streamlit.slider("Jumlah artikel yang ditampilkan", min_value=5, max_value=25, step=5)
+        
+        if streamlit.button("Cari") and user_topic != '':
+            news_list = cari_berita(topic=user_topic)
+            if news_list:
+                streamlit.subheader(f"Berikut artikel yang berkaitan dengan {user_topic.capitalize()}")
+                display_news(news_list, articles_per_page)
+            else:
+                streamlit.error(f"Artikel TIDAK Ditemukan: {user_topic}")
+        else:
+            streamlit.warning("Ketikan artikel yang ingin dicari!")
 
 if __name__ == "__main__":
     run()
